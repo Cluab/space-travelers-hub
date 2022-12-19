99dd3fba87cd39e592779e67c0c26ae7fab5c64d
Kweeka <mtalbi29@yahoo.com>
2022-12-19 10:41:40 +0100

    Add project main files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-function App() {
-	return (
-		Learn React
-	);
-}
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Rockets from './Pages/Rockets/Rockets';
+
+const App = () => (
+  <div className="app">
+    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import { NavLink } from 'react-router-dom';
+import SpaceHubLogo from '../../Assets/space-hub-logo.png';
+import './header.scss';
+
+const Header = () => {
+  const activeStyle = {
+    color: '#121212',
+  };
+
+  return (
+    <header className="header">
+      <nav>
+        <h1>
+          <img src={SpaceHubLogo} alt="Space travels' hub logo" />
+          <NavLink to="/">Space Travels&apos; Hub</NavLink>
+        </h1>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="missions"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="profile"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
