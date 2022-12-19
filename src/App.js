3c31920200c129a16179bfd0f92efcdb9bfa19b3
Kweeka <mtalbi29@yahoo.com>
2022-12-19 12:38:30 +0100

    Add logic to get list on page load

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Header from './Components/Header/Header';
 import Rockets from './Pages/Rockets/Rockets';
+import { getRockets } from './Redux/Rockets/rockets';
 
-const App = () => (
-  <div className="app">
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  dispatch(getRockets());
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
