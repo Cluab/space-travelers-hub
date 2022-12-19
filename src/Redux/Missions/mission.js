909c454bef13c08e5b6d0dfb250242bc2e8cb0bd
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-19 17:28:52 +0200

    add action and reducer for missions

---
+++ src/Redux/Missions/mission.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import getMissionsAsync from '../../Services/missionAPI';
+
+const missions = [];
+
+const GET_MISSIONS = 'spaceTravelsHub/Missions/GET_MISSIONS';
+const ADD_MISSIONS = 'spaceTravels.Hub/Missions/ADD_MISSIONS';
+
+const AddMissions = (Missions) => ({
+  type: ADD_MISSIONS,
+  payload: Missions.map((mission) => ({
+    missionName: mission.mission_name,
+    description: mission.description,
+  })),
+
+});
+
+export const getMissions = createAsyncThunk(GET_MISSIONS, async (_, thunk) => {
+  const missions = await getMissionsAsync();
+  thunk.dispatch(AddMissions(missions));
+});
+
+const MissionsReducer = (state = missions, action) => {
+  switch (action.type) {
+    case ADD_MISSIONS:
+      return [...action.payload];
+    default:
+      return state;
+  }
+};
+export default MissionsReducer;
