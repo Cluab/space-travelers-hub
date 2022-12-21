69f8ec684acb26ab5e19696e4cee888ba5bb44a9
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-21 14:27:34 +0200

    add tests for profile mission

--- src/Pages/Profile/__tests__/Profile.test.js
+++ src/Pages/Profile/__tests__/Profile.test.js
@@ -1,10 +1,13 @@
-import { render, act } from '@testing-library/react';
+import {
+  render, act, screen, fireEvent,
+} from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import '@testing-library/jest-dom/extend-expect';
 import MissionsReducer, { getMissions } from '../../../Redux/Missions/mission';
 import Profile from '../Profile';
+import MissionPage from '../../Missions/Missions';
 import rocketsReducer from '../../../Redux/Rockets/rockets';
 
 const store = configureStore({ reducer: { missions: MissionsReducer, rockets: rocketsReducer } });
@@ -19,5 +22,33 @@ describe('missions component test cases', () => {
         </Provider>,
       ).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('should find zero missions', async () => {
+    await act(() => store.dispatch(getMissions()));
+    render(
+      <Provider store={store}>
+        <Profile />
+      </Provider>,
+    );
+
+    const missionJoined = document.querySelectorAll('.missionName');
+    expect(missionJoined.length).toBe(0);
+  });
+  it('should find one missions', async () => {
+    await act(() => store.dispatch(getMissions()));
+    render(
+      <Provider store={store}>
+        <MissionPage />
+      </Provider>,
+    );
+    fireEvent.click(screen.getAllByText('Join Mission')[0]);
+    render(
+      <Provider store={store}>
+        <Profile />
+      </Provider>,
+    );
+
+    const missionJoined = document.querySelectorAll('.missionName');
+    expect(missionJoined.length).toBe(1);
   });
 });
