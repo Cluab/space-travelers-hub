3d0ce8e3034f5bbc11cc0738e715b898b65f1d41
Kweeka <mtalbi29@yahoo.com>
2022-12-21 18:18:23 +0100

    Refactor rockets reducer

--- src/Redux/Rockets/rockets.js
+++ src/Redux/Rockets/rockets.js
@@ -1,42 +1,34 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import getRocketsAsync from '../../Services/rocketAPI';
 
-const rockets = [];
-
 const GET_ROCKETS = 'spaceTravelsHub/Rockets/GET_ROCKETS';
-const ADD_ROCKETS = 'spaceTravelsHub/Rockets/ADD_ROCKETS';
-const ROCKET_RESERVATION = 'spaceTravelsHub/Rockets/ROCKET_RESERVATION';
 
-const addRockets = (rockets) => ({
-  type: ADD_ROCKETS,
-  payload: rockets.map((rocket) => ({
-    id: rocket.id,
-    rocketName: rocket.rocket_name,
-    description: rocket.description,
-    rocketImage: rocket.flickr_images[0],
-    reserved: false,
-  })),
-});
-
-export const handleReservation = (id) => ({ type: ROCKET_RESERVATION, payload: id });
-
-export const getRockets = createAsyncThunk(GET_ROCKETS, async (_, thunk) => {
+export const getRockets = createAsyncThunk(GET_ROCKETS, async () => {
   const rockets = await getRocketsAsync();
-  thunk.dispatch(addRockets(rockets));
+  return rockets;
 });
 
-const rocketsReducer = (state = rockets, action) => {
-  switch (action.type) {
-    case ADD_ROCKETS:
-      return [...action.payload];
-    case ROCKET_RESERVATION:
-      return [...state.map((rocket) => {
+const rocketSlice = createSlice({
+  name: 'rockets',
+  initialState: [],
+  reducers: {
+    handleReservation(state, action) {
+      return state.map((rocket) => {
         if (rocket.id !== action.payload) return rocket;
         return { ...rocket, reserved: !rocket.reserved };
-      })];
-    default:
-      return state;
-  }
-};
+      });
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getRockets.fulfilled, (_, action) => action.payload.map((rocket) => ({
+      id: rocket.id,
+      rocketName: rocket.rocket_name,
+      description: rocket.description,
+      rocketImage: rocket.flickr_images[0],
+      reserved: false,
+    })));
+  },
+});
 
-export default rocketsReducer;
+export default rocketSlice.reducer;
+export const { handleReservation } = rocketSlice.actions;
