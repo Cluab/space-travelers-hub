a90dd6e945f210b6bd17ccef060208914a57262e
Kweeka <mtalbi29@yahoo.com>
2022-12-21 18:18:10 +0100

    Refactor mission reducer

--- src/Redux/Missions/mission.js
+++ src/Redux/Missions/mission.js
@@ -1,45 +1,35 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import getMissionsAsync from '../../Services/missionAPI';
 
-const missions = [];
-let newState;
 const GET_MISSIONS = 'spaceTravelsHub/Missions/GET_MISSIONS';
-const ADD_MISSIONS = 'spaceTravels.Hub/Missions/ADD_MISSIONS';
-const MISSIONS_MEMBER = 'spaceTravels.Hub/Missions/JOIN_MISSIONS';
 
-const AddMissions = (Missions) => ({
-  type: ADD_MISSIONS,
-  payload: Missions.data.map((mission) => ({
-    id: mission.mission_id,
-    name: mission.mission_name,
-    description: mission.description,
-    member: false,
-  })),
-
+export const getMissions = createAsyncThunk(GET_MISSIONS, async () => {
+  const res = await getMissionsAsync();
+  return res.data;
 });
 
-export const getMissions = createAsyncThunk(GET_MISSIONS, async (_, thunk) => {
-  const missions = await getMissionsAsync();
-  thunk.dispatch(AddMissions(missions));
-});
-
-export const joinMission = (missionId) => ({
-  type: MISSIONS_MEMBER,
-  id: missionId,
-});
-
-const MissionsReducer = (state = missions, action) => {
-  switch (action.type) {
-    case ADD_MISSIONS:
-      return [...action.payload];
-    case MISSIONS_MEMBER:
-      newState = state.map((mission) => {
-        if (mission.id !== action.id) { return mission; }
+const MissionsSlice = createSlice({
+  name: 'missions',
+  initialState: [],
+  reducers: {
+    joinMission(state, action) {
+      return state.map((mission) => {
+        if (mission.id !== action.payload) { return mission; }
         return { ...mission, member: !mission.member };
       });
-      return newState;
-    default:
-      return state;
-  }
-};
-export default MissionsReducer;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getMissions.fulfilled, (_, action) => [
+      ...action.payload.map((mission) => ({
+        id: mission.mission_id,
+        name: mission.mission_name,
+        description: mission.description,
+        member: false,
+      })),
+    ]);
+  },
+});
+
+export default MissionsSlice.reducer;
+export const { joinMission } = MissionsSlice.actions;
